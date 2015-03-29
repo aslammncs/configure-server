46b20aba4d2e93fdeed5078ce94a9786a9ab2670
Mohammad Aslam <aslamm@ncs.com.sg>
2015-03-29 10:05:50 +0800

    updated

--- install.sh
+++ install.sh
@@ -12,10 +12,6 @@ if ! test -f "$chef_bin"; then
    sudo curl -L https://www.opscode.com/chef/install.sh | sudo bash &&
 fi
 
-## 4. Clone configure_server repository
-cd /tmp
-git clone https://github.com/aslammncs/configure-server.git
-
-## 5. Execute server configurations recipes
+## 3. Execute server configurations recipes
 cd /tmp/configure-server
 sudo "$chef_bin" -c solo.rb -j node.json
