7645f80d6535840bf02a52b36dd21a7ea7f3fd6b
aslam <aslamm@ncs.com.sg>
2015-04-16 07:46:53 +0000

    updated the sequence

--- cookbooks/s3downloader/recipes/default.rb
+++ cookbooks/s3downloader/recipes/default.rb
@@ -1,11 +1,21 @@
-#download latest war file from s3 bucket and install under tomcat
+# Stop tomcat before deploying War file
 
+service 'tomcat7' do
+ action :stop
+end
+
+# delete existing exploaded war directory
+directory "/var/lib/tomcat7/webapps/AslamWebApp" do
+ recursive true
+ action :delete
+end
+
+#download latest war file from s3 bucket and deploy under webapps directory
 execute "download_from_s3_and_install" do
   command "aws s3 cp --region ap-southeast-1 s3://mybuilds/AslamWebApp.war /var/lib/tomcat7/webapps/AslamWebApp.war"
 end
 
-# Restart tomcat after Deploying War file
-
+# start tomcat
 service 'tomcat7' do
- action :restart
+ action :start
 end
